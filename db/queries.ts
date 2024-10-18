fc9e6bf98e27a39f72187812e335cf00559a8185
euger <azkriven16@gmail.com>
2024-10-19 05:01:41 +0800

    added auth page

--- db/queries.ts
+++ db/queries.ts
@@ -34,115 +34,115 @@ export async function createUser(email: string, password: string) {
     }
 }
 
-export async function saveChat({
-    id,
-    messages,
-    userId,
-}: {
-    id: string;
-    messages: any;
-    userId: string;
-}) {
-    try {
-        const selectedChats = await db
-            .select()
-            .from(chat)
-            .where(eq(chat.id, id));
+// export async function saveChat({
+//     id,
+//     messages,
+//     userId,
+// }: {
+//     id: string;
+//     messages: any;
+//     userId: string;
+// }) {
+//     try {
+//         const selectedChats = await db
+//             .select()
+//             .from(chat)
+//             .where(eq(chat.id, id));
 
-        if (selectedChats.length > 0) {
-            return await db
-                .update(chat)
-                .set({
-                    messages: JSON.stringify(messages),
-                })
-                .where(eq(chat.id, id));
-        }
+//         if (selectedChats.length > 0) {
+//             return await db
+//                 .update(chat)
+//                 .set({
+//                     messages: JSON.stringify(messages),
+//                 })
+//                 .where(eq(chat.id, id));
+//         }
 
-        return await db.insert(chat).values({
-            id,
-            createdAt: new Date(),
-            messages: JSON.stringify(messages),
-            userId,
-        });
-    } catch (error) {
-        console.error("Failed to save chat in database");
-        throw error;
-    }
-}
+//         return await db.insert(chat).values({
+//             id,
+//             createdAt: new Date(),
+//             messages: JSON.stringify(messages),
+//             userId,
+//         });
+//     } catch (error) {
+//         console.error("Failed to save chat in database");
+//         throw error;
+//     }
+// }
 
-export async function deleteChatById({ id }: { id: string }) {
-    try {
-        return await db.delete(chat).where(eq(chat.id, id));
-    } catch (error) {
-        console.error("Failed to delete chat by id from database");
-        throw error;
-    }
-}
+// export async function deleteChatById({ id }: { id: string }) {
+//     try {
+//         return await db.delete(chat).where(eq(chat.id, id));
+//     } catch (error) {
+//         console.error("Failed to delete chat by id from database");
+//         throw error;
+//     }
+// }
 
-export async function getChatsByUserId({ id }: { id: string }) {
-    try {
-        return await db
-            .select()
-            .from(chat)
-            .where(eq(chat.userId, id))
-            .orderBy(desc(chat.createdAt));
-    } catch (error) {
-        console.error("Failed to get chats by user from database");
-        throw error;
-    }
-}
+// export async function getChatsByUserId({ id }: { id: string }) {
+//     try {
+//         return await db
+//             .select()
+//             .from(chat)
+//             .where(eq(chat.userId, id))
+//             .orderBy(desc(chat.createdAt));
+//     } catch (error) {
+//         console.error("Failed to get chats by user from database");
+//         throw error;
+//     }
+// }
 
-export async function getChatById({ id }: { id: string }) {
-    try {
-        const [selectedChat] = await db
-            .select()
-            .from(chat)
-            .where(eq(chat.id, id));
-        return selectedChat;
-    } catch (error) {
-        console.error("Failed to get chat by id from database");
-        throw error;
-    }
-}
+// export async function getChatById({ id }: { id: string }) {
+//     try {
+//         const [selectedChat] = await db
+//             .select()
+//             .from(chat)
+//             .where(eq(chat.id, id));
+//         return selectedChat;
+//     } catch (error) {
+//         console.error("Failed to get chat by id from database");
+//         throw error;
+//     }
+// }
 
-export async function createReservation({
-    id,
-    userId,
-    details,
-}: {
-    id: string;
-    userId: string;
-    details: any;
-}) {
-    return await db.insert(reservation).values({
-        id,
-        createdAt: new Date(),
-        userId,
-        hasCompletedPayment: false,
-        details: JSON.stringify(details),
-    });
-}
+// export async function createReservation({
+//     id,
+//     userId,
+//     details,
+// }: {
+//     id: string;
+//     userId: string;
+//     details: any;
+// }) {
+//     return await db.insert(reservation).values({
+//         id,
+//         createdAt: new Date(),
+//         userId,
+//         hasCompletedPayment: false,
+//         details: JSON.stringify(details),
+//     });
+// }
 
-export async function getReservationById({ id }: { id: string }) {
-    const [selectedReservation] = await db
-        .select()
-        .from(reservation)
-        .where(eq(reservation.id, id));
+// export async function getReservationById({ id }: { id: string }) {
+//     const [selectedReservation] = await db
+//         .select()
+//         .from(reservation)
+//         .where(eq(reservation.id, id));
 
-    return selectedReservation;
-}
+//     return selectedReservation;
+// }
 
-export async function updateReservation({
-    id,
-    hasCompletedPayment,
-}: {
-    id: string;
-    hasCompletedPayment: boolean;
-}) {
-    return await db
-        .update(reservation)
-        .set({
-            hasCompletedPayment,
-        })
-        .where(eq(reservation.id, id));
-}
+// export async function updateReservation({
+//     id,
+//     hasCompletedPayment,
+// }: {
+//     id: string;
+//     hasCompletedPayment: boolean;
+// }) {
+//     return await db
+//         .update(reservation)
+//         .set({
+//             hasCompletedPayment,
+//         })
+//         .where(eq(reservation.id, id));
+// }
